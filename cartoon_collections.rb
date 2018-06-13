76f9c248ba81e468b929eb289100cddf78b3089e
Rebecca Govert <becci.govert@gmail.com>
2018-06-13 20:06:36 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,8 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(list_words)
-  if list_words
+  list_words.any? do |word|
+    word.length > 4
 end
 
 def find_the_cheese# code an argument here
