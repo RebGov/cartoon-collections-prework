edc179acac68a1de2981d4e0446a1b4a286605fa
Rebecca Govert <becci.govert@gmail.com>
2018-06-13 20:10:07 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,8 @@ def long_planeteer_calls(list_words)
   list_words.any? { |word| word.length > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(snack_list)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  sanck_list.find { |snack| snack == cheese_types }
 end
